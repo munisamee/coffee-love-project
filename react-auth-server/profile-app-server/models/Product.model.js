7a61b9224c1c726c84e732bb44272567ab4a45f8
munisamee <munisa.ermatova@gmail.com>
2024-03-17 10:47:59 +0100

    product model is developing

---
+++ react-auth-server/profile-app-server/models/Product.model.js
@@ -0,0 +1,14 @@
+// ProductModel.js
+
+class Product {
+    constructor(name, price, description) {
+      this.name = name;
+      this.price = price;
+      this.description = description;
+    }
+  
+    // Additional methods can be added here, such as getters, setters, or utility functions.
+  }
+  
+  export default Product;
+  
